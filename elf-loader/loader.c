66aae917f04cae1862ec36d1154b9e969d7f8439
WAKABAYASHI Hiroaki <hwakabayasi@asl.cs.ritsumei.ac.jp>
2010-12-13 02:01:47 +0900

    elf-loader: build and runnable!
    - コンパイル可能，動作可能

--- elf-loader/loader.c
+++ elf-loader/loader.c
@@ -1,23 +1,44 @@
-#include <libc-symbols.h>  // for uClibc-0.9.31 (bug?)
+//#include <libc-symbols.h>  // for uClibc-0.9.31 (bug?)
 #include <elf.h>
-//#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/syscall.h>
 #include "loader.h"
+#include <fcntl.h>
 
-#define ElfW(type) struct Elf64_##type
+//#define ElfW(type) struct Elf64_##type
 
-RTLD_START
+#define MESSAGE "Hello, Dynamic Linker and Loader!\n"
+#define ERR_EXIT(err) {					\
+	  syscall(SYS_write, 2, err, sizeof(err)-1);	\
+	  syscall(SYS_exit, 1);				\
+  }
 
-static void loader_start(void)
+static void print_maps(void)
 {
-//	char buf[199];
+	int fd;
 
-//	ElfW(auxv_t) *auxv;
-//	puts("hello");
-//	syscall(SYS_write, 1, "hello, world\n", 13);
-	//extern int f(void);
-	//f();
+	fd = syscall(SYS_open, "/proc/self/maps", O_RDONLY);
+	if (fd < 0)
+		ERR_EXIT("open failed\n");
+	for (;;) {
+		char line[1024];
+		int r = syscall(SYS_read, fd, line, sizeof(line));
+		if (r == 0)
+			break;
+		if (r < 0)
+			ERR_EXIT("read error\n");
+		syscall(SYS_write, 1, line, r);
+	}
+	syscall(SYS_close, fd);
+}
+
+void loader_start(void)
+{
+	print_maps();
+
+	syscall(SYS_write, 1, MESSAGE, sizeof(MESSAGE)-1);
 	syscall(SYS_exit, 0);
+	for (;;) ;
 }
--- elf-loader/loader.h
+++ elf-loader/loader.h
@@ -1,13 +1,21 @@
 #ifndef LOADER_H
 #define LOADER_H
 
-#define RTLD_START asm ("\n\
-.text\n\
-	.align 16\n\
-.globl _start\n\
-_start:\n\
-	movq %rsp, %rdi\n\
-	call loader_start\n\
-");
+#define rtld_local __attribute__ ((visibility("hidden")))
+#define EXPORT_VAR(name, export) \
+	extern typeof(name) export \
+	__attribute__ ((alias (#name), visibility ("hidden")))
 
+#define DEFINE_GLO_VAR(type, name)   \
+	extern typeof(type) _##name; \
+	EXPORT_VAR(_##name, name);   \
+	type _##name
+
+#define DECLARE_GLO_VAR(type, name) \
+	extern typeof(type) name    \
+	__attribute__ ((visibility("hidden")))
+
+/* USAGE:
+ *   DEFINE_GLO_VAR(int, global) = 0;
+ */
 #endif
